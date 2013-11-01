7c5cc4ebfa12a73484027095edc584f86cd05f38
Joseph Kordish <joseph.kordish@mandiant.com>
2013-11-01 15:08:53 -0500

    attempt to add both period and time as variables to be passed.
    You have to do this for the SQS metrics as the default previously used would not return data points.

--- plugin/cloudwatch.js
+++ plugin/cloudwatch.js
@@ -1,30 +1,31 @@
 module.exports = function (service) {
     var aws = require ('aws-lib');
     cw = aws.createCWClient(service.from.id, service.from.secret);
-    var now = new Date,
-        start = new Date(now.getTime() - 1 * 60 * 1000); // 1 min ago
 
     return function(funneler) {
         for (var serviceName in service.services) {
             var thisService = service.services[serviceName];
             (function (serviceName, thisService) {
+                var now = new Date,
+                start = new Date(now.getTime() - thisService.time * 60 * 1000);
+                node = thisService.node;
                 cw.call(
                     "GetMetricStatistics",
                     {
                         EndTime: now.toISOString(),
                         StartTime: start.toISOString(),
+                        Period: thisService.period,
                         Namespace: thisService.namespace,
                         MetricName: thisService.metric,
                         'Statistics.member.1': thisService.type,
                         'Dimensions.member.1.Name': thisService.name,
                         'Dimensions.member.1.Value': thisService.value,
                         Unit: thisService.unit,
-                        Period: 60,
                     },
                     function(err, result) {
                         funneler({
                             'funnel': 'cloudwatch',
-                            'nodeName': thisService.value,
+                            'nodeName': thisService.node,
                             'metricName': serviceName,
                             'reading': result.GetMetricStatisticsResult.Datapoints.member[thisService.type]
                         });
